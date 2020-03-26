861528ecb373d6382345d824223da027377ed4fc
NathanJacquin <62336763+NathanJacquin@users.noreply.github.com>
2020-03-26 14:16:45 +0100

    Test

--- ex1.php
+++
@@ -1,7 +0,0 @@
-<a href="ex1.php?message=Hello">Afficher Hello</a>
-<a href="ex1.php?message=oui">Afficher oui</a>
-<a href="ex1.php?message=non">Afficher non</a>
-
-<?php
-echo "Vous êtes sur le message  ".$_GET["message"];
-?>
